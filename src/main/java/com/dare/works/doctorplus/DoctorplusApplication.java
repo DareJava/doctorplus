f8439d52627ff7c178671f0f5c98be951772aa8b
DareFat <fatimehindare@gmail.com>
2021-05-24 00:38:53 +0100

    first commit for test project

---
+++ src/main/java/com/dare/works/doctorplus/DoctorplusApplication.java
@@ -0,0 +1,13 @@
+package com.dare.works.doctorplus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoctorplusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoctorplusApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dare/works/doctorplus/DoctorplusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dare.works.doctorplus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoctorplusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
